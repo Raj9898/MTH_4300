12c0d56cac325b6cba0a8f8a3aaaea0183e601e8
Rajesh R <34293842+Raj9898@users.noreply.github.com>
2020-04-07 12:47:35 -0400

    Add files via upload

--- HW7/problem_3.cpp
+++ HW7/problem_3.cpp
@@ -1,38 +1,45 @@
 #include <iostream>
 
-
+// solves for the power of a given number
+// e.g. x^2 = x * x
 int power(int x, int a){
-int val = 1;
-for (int i = 0; i < a; ++i){
-  val *= x;
-}
-return val;
+  int val = 1;
+  for (int i = 0; i < a; ++i){
+    val *= x;
+  }
+  return val;
 }
 
 // recursive function for counting the number of swaps
 int swaps(int n, int a, int first, int sum, int k, int minK){
- int val, count=0;
+  int val, count=0;
   val = power(first, a);
-  // if the current sum is less than the assigned value we
- while (val + sum < n){
-   minK = swaps(n, a, first+1, sum+val, k+1, minK);
-   ++first;
-   val = power(first, a);
- }
+  // if the current sum is less than the assigned value
+  // we continue to check numbers itterartively
+  while (val + sum < n){
+    // we look to keep a rolling track of the minimum
+    // number of terms to a given sum
+    minK = swaps(n, a, first+1, sum+val, k+1, minK);
+    ++first;
+    val = power(first, a);
+  }
 
- // if the value matches the given sum we add one
- if (val + sum == n){
-   if (k < minK){
-     minK = k;
-   }
- }
+  // check to see if our rolling sum equals our N
+  if (val + sum == n){
+    // if the value matches the given sum we modify the
+    // minimum number of terms required to equal it
+    if (k < minK){
+      minK = k;
+    }
+  }
 
- return minK;
+  return minK;
 }
 
 int main() {
-int x, a, i, s, mK, k;
-std::cin >> x >> a;
-std::cout << swaps(x,a, i=1, s=0, k=1, mK=100) << "\n";
-return 0;
+  int x, a, i, s, mK, k;
+  std::cin >> x >> a;
+  // we initialize mK to be a very large number
+  std::cout << swaps(x,a, i=1, s=0, k=1, mK=100) << "\n";
+  return 0;
 }
